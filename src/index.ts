ace598582c2de06383b875c5e256bd4862a297d4
Ovidiu Mija <contact@askovi.com>
2020-08-13 00:01:51 -0400

    Add plugin name to platform registration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import type { API } from 'homebridge'
 
-import { PLATFORM_NAME } from './settings'
+import { PLUGIN_NAME, PLATFORM_NAME } from './settings';
 import { HomebridgeLgThinqPlatform } from './platform'
 
 /**
  * This method registers the platform with Homebridge
  */
 export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, HomebridgeLgThinqPlatform)
+  api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, HomebridgeLgThinqPlatform)
 }
