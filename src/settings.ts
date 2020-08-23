590a3e0f5b425a04813e121958d0f030a87e0b4d
Stuart Olivera <stuart@stuartolivera.com>
2020-08-23 11:18:17 -0700

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,10 +1,10 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'LgThinqAirConditioner';
-export const PLUGIN_NAME = 'homebridge-lg-thinq-ac';
+export const PLATFORM_NAME = 'LgThinqAirConditioner'
+export const PLUGIN_NAME = 'homebridge-lg-thinq-ac'
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-lg-thinq-ac';
+export const PLUGIN_NAME = 'homebridge-lg-thinq-ac'
